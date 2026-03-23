4aaef649d9d909946dafa6bce7cf2b24c6918f8a
wangjunlin03 <wangjunlin03@meituan.com>
2026-03-24 03:20:43 +0800

    fix: 修复小屏显示问题

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,3 +1,4 @@
+import useTriggerSlider from "@/hooks/use-trigger-slider";
 import { Divider, Flex, Layout, Typography } from "antd";
 
 interface Iprops {
@@ -8,9 +9,11 @@ interface Iprops {
 export default function Footer(props: Iprops) {
   const { style, className } = props;
 
+  const { isLargerThanMinWidth } = useTriggerSlider();
+
   return (
-    <Layout.Footer style={{ ...style }} className={className}>
-      <Divider style={{ fontSize: '15px', color: '#8a9099', fontWeight: 350 }}>欢迎使用，如果这个项目对您有帮助，请给我们一个 ⭐️</Divider>
+    <Layout.Footer style={{ ...style, paddingLeft: '0', paddingRight: '0' }} className={className}>
+      <Divider style={{ color: '#8a9099', padding: isLargerThanMinWidth ? '0 50px': '0 10px' }} plain>欢迎使用，如果这个项目对您有帮助，请给我们一个 ⭐️</Divider>
       <Flex justify="center">
         Isotope Lab | 同位素实验室
       </Flex>
